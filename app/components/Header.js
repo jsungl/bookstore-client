9b7295f93f60e5697533537d907f59e7de0a868b
jsungl <wotjd931026@gmail.com>
2024-03-18 01:51:07 +0900

    feat: layout setting

---
+++ app/components/Header.js
@@ -0,0 +1,27 @@
+
+export default function Hedaer() {
+    return (
+        <>
+            <div className="nav_header d-none d-md-block">
+                <div className="sidebar-header">
+                    <h3>BookStore</h3>
+                </div>
+                <div className="menu_wrap">
+                    <ul className="menu_list list-unstyled">
+                        <p>xxxjjsss</p>
+                        <li className="menu_item">
+                            <a href="#">About</a>
+                        </li>
+                        <li className="menu_item">
+                            <a href="#">Add Book</a>
+                        </li>
+                        <li className="menu_item">
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                    
+                </div>
+            </div>
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,10 +1,53 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hedaer from "./components/Header";
 
 export default function Home() {
   return (
-    <>
-      BookStore
-    </>
+    <div className="wrapper">
+      <div className="position-relative">
+        <Hedaer/>
+        <div className="content_wrapper d-none d-md-block">
+            <div className="content_header">BookStore</div>
+            <div className="content_body">
+              <button type="button" className="btn btn-primary">Button</button>
+            </div>
+            <div className="content_footer">
+              footer
+            </div>
+        </div>
+        
+        <div className="mobile_wrap d-md-none">
+          <nav className="navbar d-md-none bg-white fixed-top">
+            <div className="container-fluid">
+              <a className="navbar-brand" href="#">BookStore</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">About</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Add book</a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">Contact</a>
+                  </li>
+                </ul>            
+              </div>
+            </div>
+          </nav>
+          <div className="content_header">BookStore</div>
+          <div className="content_body">
+            <button type="button" className="btn btn-primary">Button</button>
+          </div>
+          <div className="content_footer">
+            footer
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
