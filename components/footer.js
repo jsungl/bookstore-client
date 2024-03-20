4d3399d18570508ff1fd3060dc7d5cd64af25c84
jsungl <wotjd931026@gmail.com>
2024-03-21 02:57:21 +0900

    feat: update image size

--- components/footer.js
+++ components/footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
             <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
             <div className="col-md-4 d-flex align-items-center">
                 <Link href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-                <img className="footer_logo" src="/bookstore-logo.png" alt="logo" width="30" height="24"></img>
+                <img className="footer_logo" src="/bookstore-logo.png" alt="logo" width="24" height="24"></img>
                 </Link>
                 <span className="mb-3 mb-md-0 text-body-secondary">&copy; 2024 Company, Inc</span>
             </div>
