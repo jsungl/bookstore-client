428e093fd461822231a7a5bf845cb1f122a17c4c
jsungl <wotjd931026@gmail.com>
2024-03-26 01:49:40 +0900

    feat: add search book function
    검색 기능 추가

---
+++ app/search/page.js
@@ -0,0 +1,60 @@
+"use client"
+
+import Card from "@/components/card";
+import { getRequest } from "@/services/bookservice";
+import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Search() {
+
+    const [bookList, setBookList] = useState([]);
+    const [refresh, setRefresh] = useState(false);
+    const searchParams = useSearchParams();
+    const keyword = searchParams.get('keyword');
+
+    useEffect(()=>{
+        findBooks();
+    },[refresh])
+
+    async function findBooks() {
+        const allbooks = await getRequest("/search?keyword=" + keyword);
+        setBookList(allbooks);
+    }
+
+    function isRefreshRequired() {
+        setRefresh(!refresh);
+    }
+
+    return (
+        <>
+            {bookList.length !== 0 ?
+                <div className="container">
+                    <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 my-3 g-3">
+                        {
+                            bookList.map((book,key)=>{
+                                return (
+                                    <div className="col" key={key}>
+                                        <Card bookId={book.bookId}
+                                            bookTitle={book.title}
+                                            bookDesc={book.description}
+                                            bookImageUrl={book.imageUrl}
+                                            bookPrice={book.price}
+                                            isRefreshRequired={isRefreshRequired}
+                                        />
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+                :
+                <div className="container empty">
+                    <div className="d-flex justify-content-center align-items-center h-100">
+                        <div><i className="bi bi-exclamation-circle"></i></div>
+                        <div className="ms-2">No Books</div>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
--- components/header.js
+++ components/header.js
@@ -1,10 +1,23 @@
+"use client"
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Header() {
+
+    const [keyword, setKeyword] = useState("");
+    const router = useRouter();
+
+    function onSubmitHandler(e) {
+        e.preventDefault();
+        router.push("/search?keyword=" + keyword);
+    }
+
     return (
         <div className="container">
-            <form className="d-flex" role="search">
-            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-            <button className="btn btn-outline-success" type="submit">Search</button>
+            <form className="d-flex" onSubmit={(e)=>{onSubmitHandler(e)}}>
+                <input className="form-control me-2" type="text" placeholder="Search" value={keyword} onChange={(e)=>setKeyword(e.target.value)}/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
             </form>
         </div>
     )
