8209ba2a7273a55e59703dc3060f26c0136ba608
jsungl <wotjd931026@gmail.com>
2024-04-07 19:18:50 +0900

    feat: add image replace function
    image url 로부터 이미지를 가져오지 못할 경우 대체 이미지 표시

--- app/book/[id]/page.js
+++ app/book/[id]/page.js
@@ -1,30 +1,35 @@
 "use client"
 
-import { getRequest } from "@/services/bookservice";
+import Loading from "@/app/loading";
+import { useParams } from "next/navigation"
 import { useEffect, useState } from "react";
 
 
-export default function Book(props) {
-    const bookId = props.params.id;
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
-    const [price, setPrice] = useState(0);
+export default function Book() {
+    
+    const params = useParams();
+    const [book, setBook] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+    const [message, setMessage] = useState("");
 
-    useEffect(() => {
-        getBookById();
+    useEffect(()=>{
+        fetchBook();
     },[])
 
-    async function getBookById() {
-        const response = await getRequest("/api/books/" + bookId);
-        const book = response.data.book;
+    async function fetchBook() {
+        const res = await fetch(`http://127.0.0.1:8081/api/books/${params.id}`,{ cache: 'no-store' });
+        const result = await res.json();
 
-        setTitle(book.title);
-        setDescription(book.description);
-        setImageUrl(book.imageUrl);
-        setPrice(book.price);
+        if(!result.success) {
+            setIsError(true);
+            setMessage(result.data.error.errorMessage);
+        } else {
+            setBook(result.data.book);
+            setLoading(false);
+        }
+
     }
-
 
     function onAddCartButtonClickHandler() {
         alert("Service not yet!");
@@ -34,52 +39,71 @@ export default function Book(props) {
         alert("Service not yet!");
     }
 
+    const replaceImg = (e) => {
+        e.target.onerror = null;
+        e.target.src = '/not_exist_default.jpg';
+    }
 
     return (
-        <div className="container">
-            <h3 className="text-center my-5">Book Information</h3>
-            <div className="d-flex flex-column mb-3">
-                <div className="row row-cols-md-2 row-cols-sm-1 row-cols-1">
-                    <div className="col-lg-4 col-md-12">
-                        <div className="d-flex justify-content-center">
-                            <img src={imageUrl} className="bd-img" alt="book image"/>
+        <>
+            {
+                isError ?
+                (
+                <div className="container">    
+                <div className="alert alert-danger" role="alert">
+                    <h4>요청실패</h4>
+                    <h6>에러 메시지: {message}</h6>
+                </div>
+                </div>)
+                :
+                (
+                loading ? <Loading/> :
+                <div className="container">
+                    <h3 className="text-center my-5">Book Information</h3>
+                    <div className="d-flex flex-column mb-3">
+                        <div className="row row-cols-md-2 row-cols-sm-1 row-cols-1">
+                            <div className="col-lg-4 col-md-12">
+                                <div className="d-flex justify-content-center">
+                                    <img src={book.imageUrl} className="bd-img" alt="book image" onError={replaceImg} style={{maxWidth:'400px'}}/>
+                                </div>
+                            </div>
+                            <div className="col-lg-8 col-md-12">
+                                <div className="p-1 fs-4 border-bottom">
+                                    <p>{book.title}</p>
+                                </div>
+                                <div className="p-1"><p>{book.description}</p></div>
+                                <div className="d-flex flex-column mt-5 p-1" style={{ width: '15rem' }}>
+                                    <div>
+                                        <h5>Total: ${book.price}</h5>
+                                    </div>
+                                    <select className="form-select" aria-label="Default select example" defaultValue={0}>
+                                        <option value={0}>Quantity</option>
+                                        <option value={1}>1</option>
+                                        <option value={2}>2</option>
+                                        <option value={3}>3</option>
+                                    </select>
+                                    <div className="mt-2">
+                                        <button type="button" className="btn btn-primary me-3" style={{ background: '#FFD814',borderColor:'#FCD200'}}
+                                                onClick={()=>onAddCartButtonClickHandler()}>Add to Cart</button>
+                                        <button type="button" className="btn btn-primary" style={{ background: '#FFA41C',borderColor:'#FF8F00'}}
+                                                onClick={()=>onBuyButtonClickHandler()}>Buy Now</button>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-lg-8 col-md-12">
-                        <div className="p-1 fs-4 border-bottom">
-                            <p>{title}</p>
-                        </div>
-                        <div className="p-1"><p>{description}</p></div>
-                        <div className="d-flex flex-column mt-5 p-1" style={{ width: '15rem' }}>
+                        <hr className="my-5"/>
+                        {/* <div className="container empty"> */}
+                        <div className="container">  
                             <div>
-                                <h5>Total: ${price}</h5>
+                                <h5>Description</h5>
+                                <div>{book.description}</div>
                             </div>
-                            <select className="form-select" aria-label="Default select example" defaultValue={0}>
-                                <option value={0}>Quantity</option>
-                                <option value={1}>1</option>
-                                <option value={2}>2</option>
-                                <option value={3}>3</option>
-                            </select>
-                            <div className="mt-2">
-                                <button type="button" className="btn btn-primary me-3" style={{ background: '#FFD814',borderColor:'#FCD200'}}
-                                        onClick={()=>onAddCartButtonClickHandler()}>Add to Cart</button>
-                                <button type="button" className="btn btn-primary" style={{ background: '#FFA41C',borderColor:'#FF8F00'}}
-                                        onClick={()=>onBuyButtonClickHandler()}>Buy Now</button>
-                            </div>
+                            
                         </div>
                     </div>
-                </div>
-                <hr className="my-5"/>
-                <div className="container empty">
-                    <div>
-                        <h5>Description</h5>
-                        <div>{description}</div>
-                    </div>
-                    
-                </div>
-            </div>
-            
-            
-        </div>
+                        
+                </div>)
+            }
+        </>
     )
 }
