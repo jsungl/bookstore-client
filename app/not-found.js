7d9e48d87c36738c2d5de918b04d1559c42bebc3
jsungl <wotjd931026@gmail.com>
2024-04-07 18:12:13 +0900

    feat: add exception handling page

---
+++ app/not-found.js
@@ -0,0 +1,16 @@
+
+export default function NotFound() {
+
+    return (
+        <div className="container d-flex align-items-center justify-content-center h-100">
+            <div className="d-flex flex-column text-center">
+                <div>
+                    <h2>Page not found</h2>
+                </div>
+                <div>
+                    <p>Could not find requested resource.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
