810ce75f029aede2482bcbcbeb2af02889848be4
jsungl <wotjd931026@gmail.com>
2024-04-14 23:43:21 +0900

    feat: add side nav component
    로그인 유무를 위해 내 정보 조회 API 추가, 회원가입 탭 추가

---
+++ components/sidenav.js
@@ -0,0 +1,72 @@
+'use client'
+
+import { useGlobalContext } from "@/app/context/store";
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { useEffect } from "react";
+
+export default function SideNav() {
+    const pathname = usePathname();
+
+    const { user, setUser } = useGlobalContext();
+    //console.log(Object.keys(user).length);
+
+    useEffect(()=>{
+      console.log("Side Nav Component 렌더링!");
+      if(Object.keys(user).length == 0) {
+        fetchUser();
+      }
+    },[])
+
+    async function fetchUser() {
+      const response = await fetch("http://127.0.0.1:8081/api/members/me", {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json' 
+        }
+      });
+
+      const result = await response.json();
+      //console.log(result);
+      if(result.success) {
+        setUser(result.data.member);
+      }
+
+    }
+
+
+    return (
+        <nav className="navbar navbar-expand-md bg-dark navbar-dark align-items-start">
+        <div className="container-fluid">
+          <div className="logo_brand d-flex align-items-center justify-content-between">
+              <div className="logo d-flex align-items-center justify-content-center">
+                  <Link href="/">
+                      <img src="/bookstore-removebg-logo.png" alt="logo"></img>
+                  </Link>
+              </div>
+          </div>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {
+            Object.keys(user).length !== 0 && <span className="text-white">User</span>
+          }
+          <div className="collapse navbar-collapse w-100" id="navbarSupportedContent">
+            <hr className="border border-1 w-100"/>
+            <ul className="navbar-nav nav-pills me-auto mb-2 mb-lg-0 w-100">
+              <li className="nav-item">
+                <Link href="/addBook" className={pathname === '/addBook' ? 'nav-link active' : 'nav-link'}>Add Book</Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/login" className={pathname === '/login' ? 'nav-link active' : 'nav-link'}>Login</Link>
+              </li>
+              <li className="nav-item">
+                <Link href="/register" className={pathname === '/register' ? 'nav-link active' : 'nav-link'}>Register</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </nav>
+    )
+}
