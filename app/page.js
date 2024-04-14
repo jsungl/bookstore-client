38019cbaf9e562a992b012a8df3c826559f6170c
jsungl <wotjd931026@gmail.com>
2024-04-14 23:37:04 +0900

    feat: update home page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@
 import Card from "@/components/card";
 import { useEffect, useState } from "react";
 import Loading from "./loading";
+import SideBar from "@/components/sidebar";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import SideNav from "@/components/sidenav";
 
 export default function Home() {
   const [bookList, setBookList] = useState([]);
@@ -10,7 +14,13 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
-    fetch("http://127.0.0.1:8081/api/books",{ cache: 'no-store' })
+    fetch("http://127.0.0.1:8081/api/books",{ 
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+          'Content-Type': 'application/json'
+      } 
+    })
     .then(res=>res.json())
     .then(res=>{
       setBookList(res.data.books);
@@ -24,39 +34,48 @@ export default function Home() {
 
   return (
     <>
-      
-      <div className="container">
-        { 
-          loading ? <Loading/>
-          :
-          (bookList.length !== 0 ?
-              <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 my-3 g-3">
-                {
-                  bookList.map((book,key)=>{
-                    return (
-                      <div className="col" key={key}>
-                        <Card bookId={book.bookId}
-                              bookTitle={book.title}
-                              bookImageUrl={book.imageUrl}
-                              bookPrice={book.price}
-                              isRefreshRequired={isRefreshRequired}
-                        />
-                      </div>
-                    )
-                  })
-                }
-              </div>
-            :
-            <div className="container h-100">
-              <div className="d-flex justify-content-center align-items-center h-100">
-                <div><i className="bi bi-exclamation-circle"></i></div>
-                <div className="ms-2">No Books</div>
-              </div>
-            </div>
-          )
-        }
+      <SideNav/>
+      <div className="content_main d-flex flex-column">
+        <div className="content_header d-flex align-items-center">
+          <Header/>
+        </div>
+        <div className="content_body flex-grow-1">
+          <div className="container">
+            { 
+              loading ? <Loading/>
+              :
+              (bookList.length !== 0 ?
+                  <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 my-3 g-3">
+                    {
+                      bookList.map((book,key)=>{
+                        return (
+                          <div className="col" key={key}>
+                            <Card bookId={book.bookId}
+                                  bookTitle={book.title}
+                                  bookImageUrl={book.imageUrl}
+                                  bookPrice={book.price}
+                                  isRefreshRequired={isRefreshRequired}
+                            />
+                          </div>
+                        )
+                      })
+                    }
+                  </div>
+                :
+                <div className="container h-100">
+                  <div className="d-flex justify-content-center align-items-center h-100">
+                    <div><i className="bi bi-exclamation-circle"></i></div>
+                    <div className="ms-2">No Books</div>
+                  </div>
+                </div>
+              )
+            }
+          </div>
+        </div>
+        <div className="content_footer">
+          <Footer/>
+        </div>
       </div>
-        
     </>
   );
 }
