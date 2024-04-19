8fb8802c14f10508f97f5c5c2cfa96f55a7d7cb5
jsungl <wotjd931026@gmail.com>
2024-04-19 19:33:41 +0900

    feat: add logout handler, loading component
    로그아웃 버튼 핸들러 추가, 로딩 화면 표시

--- components/sidenav.js
+++ components/sidenav.js
@@ -2,18 +2,21 @@
 
 import { useGlobalContext } from "@/app/context/store";
 import Link from "next/link"
-import { usePathname } from "next/navigation"
-import { useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation"
+import { useEffect, useState } from "react";
 
 export default function SideNav({hasToken}) {
     const pathname = usePathname();
     const { user, setUser } = useGlobalContext();
+    const [loading, setLoading] = useState(true);
+    const router = useRouter();
 
     useEffect(()=>{
-      console.log(hasToken);
+      //console.log(hasToken);
       if(hasToken && Object.keys(user).length == 0) {
         fetchUser();
       }
+      setLoading(false);
     },[]);
 
     async function fetchUser() {
@@ -34,6 +37,24 @@ export default function SideNav({hasToken}) {
 
     }
 
+    async function onLogoutButtonClickHandler() {
+      if(confirm("Do you want to logout?")){
+
+        const response = await fetch("http://localhost:8081/api/members/logout", {
+          method: 'POST',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json' 
+          }
+        });
+        const result = await response.json();
+        if(result.success) {
+          setUser({});
+          router.replace("/");
+        }
+      }
+    }
+
 
     return (
         <nav className="navbar navbar-expand-md bg-dark navbar-dark align-items-start">
@@ -48,30 +69,41 @@ export default function SideNav({hasToken}) {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          {
-            Object.keys(user).length !== 0 && <span className="text-white">User</span>
-          }
+          
           <div className="collapse navbar-collapse w-100" id="navbarSupportedContent">
             <hr className="border border-1 w-100"/>
-            <ul className="navbar-nav nav-pills me-auto mb-2 mb-lg-0 w-100">
-              {
+            {
+              loading ? <ul className="navbar-nav nav-pills me-auto mb-2 mb-lg-0 w-100"></ul> :
+                
                 Object.keys(user).length !== 0 ?
-                <li className="nav-item">
-                  <Link href="/addBook" className={pathname === '/addBook' ? 'nav-link active' : 'nav-link'}>Add Book</Link>
-                </li>
-                :
-                <>
+                <ul className="navbar-nav nav-pills me-auto mb-2 mb-lg-0 w-100">
+                  <div>
+                    <span className="text-white px-2">hello, {user.nickname}</span>
+                    <button type="button" className="btn btn-outline-light btn-sm" onClick={()=>onLogoutButtonClickHandler()}>logout</button>
+                  </div>
+                  <hr className="border border-1 w-100"/>
                   <li className="nav-item">
-                    <Link href="/login" className={pathname === '/login' ? 'nav-link active' : 'nav-link'}>Login</Link>
+                    <Link href="/addbook" className={pathname === '/addbook' ? 'nav-link active' : 'nav-link'}>Add Book</Link>
                   </li>
+                  <li className="nav-item">
+                    <Link href="/mypage" className={pathname === '/mypage' ? 'nav-link active' : 'nav-link'}>MyPage</Link>
+                  </li>
+                </ul>
+                :
+                <ul className="navbar-nav nav-pills me-auto mb-2 mb-lg-0 w-100">
                   <li className="nav-item">
                     <Link href="/register" className={pathname === '/register' ? 'nav-link active' : 'nav-link'}>Register</Link>
                   </li>
-                </>
-              }
-            </ul>
+                  <li className="nav-item">
+                    <Link href="/login" className={pathname === '/login' ? 'nav-link active' : 'nav-link'}>Login</Link>
+                  </li>
+                </ul>
+            }
+      
           </div>
+          
         </div>
+        
         </nav>
     )
 }
