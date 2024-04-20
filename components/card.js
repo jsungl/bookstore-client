4066b507bbdaad4ee634773d76429c60e51a4cb5
jsungl <wotjd931026@gmail.com>
2024-04-21 00:20:40 +0900

    feat: update update,delete button
    관리자 로그인시 모든 게시물들의 수정,삭제 버튼 표시

--- components/card.js
+++ components/card.js
@@ -1,93 +1,150 @@
-"use client"
+"use client";
 
 import { useGlobalContext } from "@/app/context/store";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
-
 export default function Card(props) {
-    const {bookId, bookTitle, bookImageUrl, bookPrice, bookSeller, isRefreshRequired} = props;
-    const router = useRouter();
-    const { user } = useGlobalContext();
+  const {
+    bookId,
+    bookTitle,
+    bookImageUrl,
+    bookPrice,
+    bookSeller,
+    isRefreshRequired,
+  } = props;
+  const router = useRouter();
+  const { user } = useGlobalContext();
 
-    function onUpdateButtonClickHandler() {
-        // router.push("/edit/" + bookId);
-        router.push(`/book/${bookId}/edit`);
+  function onUpdateButtonClickHandler() {
+    // router.push("/edit/" + bookId);
+    router.push(`/book/${bookId}/edit`);
+  }
+
+  async function onDeleteButtonClickHandler() {
+    const response = await fetch(`http://localhost:8081/api/books/${bookId}`, {
+      method: "DELETE",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      isRefreshRequired();
+    } else {
+      alert("delete fail!");
     }
+  }
 
-    async function onDeleteButtonClickHandler() {
-        const response = await fetch(`http://localhost:8081/api/books/${bookId}`, {
-            method: 'DELETE',
-            credentials: 'include',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
+  const replaceImg = (e) => {
+    e.target.onerror = null;
+    e.target.src = "/not_exist_default.jpg";
+  };
 
-        if(response.ok) {
-            isRefreshRequired();
-        } else {
-            alert('delete fail!');
-        }
-    }
-
-    const replaceImg = (e) => {
-        e.target.onerror = null;
-        e.target.src = '/not_exist_default.jpg';
-    }
-
-    return (
-        <>
-            <div className="card">
-                <div className="d-flex justify-content-center" style={{ background: '#eee' }}>
-                    <Link href={`/book/${bookId}`}>
-                        <img src={bookImageUrl} className="bd-img card-img-top p-3" alt="book image" onError={replaceImg}/>
-                    </Link>
-                </div>
-                <div className="card-body">
-                    <div className="d-flex flex-column justify-content-between" style={{ height:'190px'}}>
-                        <div>
-                            <Link href={`/book/${bookId}`} className="text-decoration-none link-dark">
-                                <h5 className="card-title">
-                                    {bookTitle}
-                                </h5>
-                            </Link>
-                            
-                            <p className="card-text">
-                                ${bookPrice}
-                            </p>
-                        </div>
-                        {Object.keys(user).length !== 0 && user.id === bookSeller ?
-                            <div className="d-flex justify-content-end">
-                                <button type="button" className="btn btn-sm btn-light me-2" style={{ borderColor:'#adb5bd' }} onClick={()=>onUpdateButtonClickHandler()}>update</button>
-                                <button type="button" className="btn btn-sm btn-danger" data-bs-toggle="modal" data-bs-target={`#modalId-${bookId}`}>delete</button>
-                            </div>
-                            :
-                            <></>
-                        }
-                    </div>
-                </div>
-                    
-            </div>
-
-            {/* Modal */}
-            <div className="modal fade" id={`modalId-${bookId}`} aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div className="modal-dialog">
-            <div className="modal-content">
-                <div className="modal-header">
-                <h1 className="modal-title fs-5" id="exampleModalLabel">Delete Book</h1>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                    Are you sure you want to delete <span className="fw-bold">{bookTitle}?</span>
-                </div>
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={()=>onDeleteButtonClickHandler()}>Yes</button>
-                    <button type="button" className="btn btn-primary" data-bs-dismiss="modal">No</button>
-                </div>
-            </div>
-            </div>
+  return (
+    <>
+      <div className="card">
+        <div
+          className="d-flex justify-content-center"
+          style={{ background: "#eee" }}
+        >
+          <Link href={`/book/${bookId}`}>
+            <img
+              src={bookImageUrl}
+              className="bd-img card-img-top p-3"
+              alt="book image"
+              onError={replaceImg}
+            />
+          </Link>
         </div>
-      </>
-    )
+        <div className="card-body">
+          <div
+            className="d-flex flex-column justify-content-between"
+            style={{ height: "190px" }}
+          >
+            <div>
+              <Link
+                href={`/book/${bookId}`}
+                className="text-decoration-none link-dark"
+              >
+                <h5 className="card-title">{bookTitle}</h5>
+              </Link>
+
+              <p className="card-text">${bookPrice}</p>
+            </div>
+            {Object.keys(user).length !== 0 ? (
+              (user.id === bookSeller || user.role === "ADMIN") && (
+                <div className="d-flex justify-content-end">
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-light me-2"
+                    style={{ borderColor: "#adb5bd" }}
+                    onClick={() => onUpdateButtonClickHandler()}
+                  >
+                    update
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-sm btn-danger"
+                    data-bs-toggle="modal"
+                    data-bs-target={`#modalId-${bookId}`}
+                  >
+                    delete
+                  </button>
+                </div>
+              )
+            ) : (
+              <></>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id={`modalId-${bookId}`}
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Delete Book
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              Are you sure you want to delete{" "}
+              <span className="fw-bold">{bookTitle}?</span>
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+                onClick={() => onDeleteButtonClickHandler()}
+              >
+                Yes
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-dismiss="modal"
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
