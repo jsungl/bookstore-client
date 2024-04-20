ab95bc2d131136b968914925563fd36bdd0b3e5d
jsungl <wotjd931026@gmail.com>
2024-04-21 00:15:44 +0900

    feat: update error page

--- app/error.js
+++ app/error.js
@@ -1,21 +1,32 @@
-"use client"
+"use client";
 
-import Link from "next/link"
+import Link from "next/link";
 //import { useEffect } from "react"
 
-export default function Error({error, reset}) {
+export default function Error({ error, reset }) {
+  // useEffect(() => {
+  //     console.error(error)
+  // }, [error])
 
-    // useEffect(() => {
-    //     console.error(error)
-    // }, [error])
-
-    return(
-        <div>
-            <h2>Something went wrong!</h2>
-            <button onClick={() => reset()}>
-                Try again
-            </button>
-            <Link href="/">Back to Home</Link>
-        </div>
-    )
+  return (
+    <div className="container text-center">
+      <h2>This Page isn't working</h2>
+      <p>If the problem is continues, contact the site owner</p>
+      <p>
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={() => reset()}
+        >
+          Try again
+        </button>
+      </p>
+      <Link
+        href="/"
+        className="link-secondary link-underline-opacity-0 link-underline-opacity-75-hover"
+      >
+        Back to Home
+      </Link>
+    </div>
+  );
 }
