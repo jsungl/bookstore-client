eb46b2b0c95f622902f8c31c3dda712de1dbdfcb
jsungl <wotjd931026@gmail.com>
2024-04-27 00:58:10 +0900

    feat: add display search keyword
    URL 입력하여 접속시 검색어 표시

--- components/header.js
+++ components/header.js
@@ -1,24 +1,42 @@
-"use client"
+"use client";
 
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const [keyword, setKeyword] = useState("");
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("query");
 
-    const [keyword, setKeyword] = useState("");
-    const router = useRouter();
+  useEffect(() => {
+    search === null ? setKeyword("") : setKeyword(search);
+  }, [search]);
 
-    function onSubmitHandler(e) {
-        e.preventDefault();
-        router.push("/search?query=" + keyword);
-    }
+  function onSubmitHandler(e) {
+    e.preventDefault();
+    router.push("/search?query=" + keyword, { scroll: false });
+  }
 
-    return (
-        <div className="container py-3">
-            <form className="d-flex" onSubmit={(e)=>{onSubmitHandler(e)}}>
-                <input className="form-control me-2" type="text" placeholder="Search by title" value={keyword} onChange={(e)=>setKeyword(e.target.value)}/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-        </div>
-    )
+  return (
+    <div className="container py-3">
+      <form
+        className="d-flex"
+        onSubmit={(e) => {
+          onSubmitHandler(e);
+        }}
+      >
+        <input
+          className="form-control me-2"
+          type="text"
+          placeholder="Search by title"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
 }
