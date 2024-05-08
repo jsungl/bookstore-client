030ddf162ce4cf8451942d3a735545e445fc372c
jsungl <wotjd931026@gmail.com>
2024-05-09 02:58:13 +0900

    feat: update layout

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import Card from "@/components/card";
 import { useEffect, useState } from "react";
 // import { getCookie } from 'cookies-next';
 import Loading from "@/components/loading";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export default function Home() {
   const [bookList, setBookList] = useState([]);
@@ -37,37 +39,47 @@ export default function Home() {
     return <Loading />;
   } else {
     return (
-      <div className="container">
-        {bookList.length !== 0 ? (
-          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 my-3 g-3">
-            {bookList.map((book, key) => {
-              return (
-                <div className="col" key={key}>
-                  <Card
-                    bookId={book.bookId}
-                    bookTitle={book.title}
-                    bookImageUrl={book.imageUrl}
-                    bookPrice={book.price}
-                    bookSeller={book.sellerId}
-                    isRefreshRequired={isRefreshRequired}
-                  />
-                </div>
-              );
-            })}
-          </div>
-        ) : (
-          isFetch && (
-            <div className="container h-100">
-              <div className="d-flex justify-content-center align-items-center h-100">
-                <div>
-                  <i className="bi bi-exclamation-circle"></i>
-                </div>
-                <div className="ms-2">No Books</div>
+      <>
+        <div className="content_header d-flex align-items-center">
+          <Header />
+        </div>
+        <div className="content_body flex-grow-1">
+          <div className="container">
+            {bookList.length !== 0 ? (
+              <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-2 row-cols-xl-3 row-cols-xxl-4 my-3 g-3">
+                {bookList.map((book, key) => {
+                  return (
+                    <div className="col" key={key}>
+                      <Card
+                        bookId={book.bookId}
+                        bookTitle={book.title}
+                        bookImageUrl={book.imageUrl}
+                        bookPrice={book.price}
+                        bookSeller={book.sellerId}
+                        isRefreshRequired={isRefreshRequired}
+                      />
+                    </div>
+                  );
+                })}
               </div>
-            </div>
-          )
-        )}
-      </div>
+            ) : (
+              isFetch && (
+                <div className="container h-100">
+                  <div className="d-flex justify-content-center align-items-center h-100">
+                    <div>
+                      <i className="bi bi-exclamation-circle"></i>
+                    </div>
+                    <div className="ms-2">No Books</div>
+                  </div>
+                </div>
+              )
+            )}
+          </div>
+        </div>
+        <div className="content_footer">
+          <Footer />
+        </div>
+      </>
     );
   }
 }
