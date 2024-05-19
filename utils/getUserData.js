dfe32d168c93da5255dd4f39ac831c35deccc541
jsungl <wotjd931026@gmail.com>
2024-05-19 15:24:31 +0900

    feat: restore backend api url

--- utils/getUserData.js
+++ utils/getUserData.js
@@ -1,13 +1,5 @@
 const getUserData = async () => {
-  // const response = await fetch("/api/members/me", {
-  //   method: "GET",
-  //   credentials: "include",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  // });
-
-  const response = await fetch(`${process.env.API_URL}/api/members/me`, {
+  const response = await fetch("/api/members/me", {
     method: "GET",
     credentials: "include",
     headers: {
