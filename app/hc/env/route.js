a9623f15863045a7dd2cbdec6b2d10ef3463e013
jsungl <wotjd931026@gmail.com>
2024-05-30 14:19:33 +0900

    feat: update route handlers for deploy

---
+++ app/hc/env/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const env = process.env.ENV;
+    // return new NextResponse(env, {
+    //     status: 200,
+    //     headers: {
+    //         'Content-Type': 'text/plain'
+    //     }
+    // });
+
+    return new Response(env, {
+        status:200,
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    });
+}
